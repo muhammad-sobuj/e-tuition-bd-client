40418890eb0f331c1570d9fa2c033e50c1995e07
muhammadsobuj20 <muhammadsobuj20@gmail.com>
2025-12-15 23:32:02 +0600

    added a userole

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -1,46 +1,24 @@
-import axios from 'axios'
-import { useQuery } from '@tanstack/react-query'
-import { useAuth } from '../Context/AuthContext'
-
-
-
-// const useRole = () => {
-//   const { user, loading } = useAuth()
-
-
-//   const { data: role, isLoading: isRoleLoading } = useQuery({
-//     enabled: !loading && !!user?.email,
-//     queryKey: ['role', user?.email],
-//     queryFn: async () => {
-//       const result = await axios.get(`/user/role`)
-//       console.log(result)
-//       return result.data.role
-//     },
-//   })
-
-//   //   return { role, isRoleLoading }
-//   return [role, isRoleLoading]
-// }
-
-// export default useRole
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "../Context/AuthContext";
 
 const useRole = () => {
-  const { user, loading } = useAuth()
+  const { user, loading } = useAuth();
 
   const { data: role = null, isLoading: isRoleLoading } = useQuery({
     enabled: !loading && !!user?.email,
-    queryKey: ['role', user?.email],
+    queryKey: ["role", user?.email],
     queryFn: async () => {
-      const token = await user.getIdToken()
+      const token = await user.getIdToken();
       const { data } = await axios.get(
         `${import.meta.env.VITE_API_URL}/users/role`,
         { headers: { authorization: `Bearer ${token}` } }
-      )
-      return data.role
+      );
+      return data.role;
     },
-  })
+  });
 
-  return [role, isRoleLoading]
-}
+  return [role, isRoleLoading];
+};
 
-export default useRole
+export default useRole;
